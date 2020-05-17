df825cc80e8637c67f5caf72f10e69bdc5e04b12
Juan Miguel Marqués Morilla <marques.jm@icloud.com>
2020-05-17 20:50:57 +0200

    First initialization project

---
+++ Covid-19/Covid-19/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Covid-19
+//
+//  Created by Juan Miguel Marques on 17/05/2020.
+//  Copyright © 2020 Juan Miguel Marques. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
